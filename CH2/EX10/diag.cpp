826031e090bdd15f1cd633a2c81c6361f410ccc0
Zhuan-pitt <70167028+Zhuan-pitt@users.noreply.github.com>
2020-09-08 16:05:54 -0400

    new updates

--- CH2/EX10/diag.cpp
+++ CH2/EX10/diag.cpp
@@ -36,23 +36,83 @@ int main (int argc, char **argv)
     a=-a;b=-b;c=-c;
 
 
-    Complex deta0 = pow((b/3.-pow(a,2)/9.),3);
-    Complex deta1 = a*b/6. - pow(a,3)/27.-c/2.;
+    Complex delta0 = pow(a,2)-3.*b;
+    Complex delta1 = 2.*pow(a,3)-9.*a*b+27.*c;
 
-    Complex ci(-0.5,sqrt(3)/2.);
-    Complex ci_t(-0.5,-sqrt(3)/2.);
+    Complex ci(-0.5,sqrt(3.)/2.);
+    Complex ci_t(-0.5,-sqrt(3.)/2.);
 
-    Complex u1 =pow(deta1+pow(pow(deta1,2)+deta0,0.5),1./3.);
+    Complex C=pow((delta1+pow(pow(delta1,2)-4.*pow(delta0,3),1/2.))/2.,1/3.);
 
-    Complex u2 =pow(deta1-pow(pow(deta1,2)+deta0,0.5),1./3.);
 
-    Complex x1 = -a/3. + u1+u2 ;
-    Complex x2 = -a/3. + ci*u1 +ci_t*u2;
-    Complex x3 = -a/3. + ci*u2 +ci_t*u1;
+    Complex x1 = -1./3.*(a+C+delta0/C) ;
+    Complex x2 = -1./3.*(a+ci*C+ci_t*delta0/C) ;
+    Complex x3 = -1./3.*(a+ci_t*C+ci*delta0/C) ;
     
-    std::cout<<a<<" "<<b<<" "<<c<<std::endl;
-    std::cout<<x1<<" "<<x2<<" "<<x3<<std::endl;
+    //std::cout<<x1<<" "<<x2<<" "<<x3<<std::endl;
 
+    Vector3cd Eig;
+    Eig << x1, x2, x3 ;
+
+    std::cout<<"The eigenvalue of matrix A "<< " is "<< Eig.transpose()<<std::endl;
+    Matrix3cd Eigvec;
+    for (int i = 0; i<=2; i++){
+        Matrix3cd B=A;
+        B(0,0)=B(0,0)-Eig(i);
+        B(1,1)=B(1,1)-Eig(i);
+        B(2,2)=B(2,2)-Eig(i);
+
+        Matrix2cd b1;
+        Matrix2cd b2;
+        Matrix2cd b3;
+        Matrix2cd b4;
+
+        b1 << B(0,0),B(0,1),
+              B(1,0),B(1,1);
+        b2 << B(0,1),B(0,2),
+              B(1,1),B(1,2);
+        b3 << B(0,0),B(0,1),
+              B(2,0),B(2,1);
+        b4 << B(0,1),B(0,2),
+              B(2,1),B(2,2);
+
+        Complex de = b1.determinant()*b4.determinant()-b2.determinant()*b3.determinant();
+        
+
+        if (abs(de)<pow(10,-8)){
+            Eigvec(i,0)=1;
+            Eigvec(i,1)=b1.determinant()/b2.determinant();
+            Eigvec(i,2)=-(B(0,0)*Eigvec(i,0)+B(0,2)*Eigvec(i,2))/B(0,1);
+        }
+        else{
+            Eigvec(i,0)=0;
+            Eigvec(i,1)=0;
+            Eigvec(i,2)=1;
+        }
+
+        
+    }
+
+    //normalize the eigenvectors
+
+    Vector3cd v1;
+    Vector3cd v2;
+    Vector3cd v3;
+
+    v1<<Eigvec(0,0),Eigvec(1,0),Eigvec(2,0);
+    v2<<Eigvec(0,1),Eigvec(1,1),Eigvec(2,1);
+    v3<<Eigvec(0,2),Eigvec(1,2),Eigvec(2,2);
+
+    v1.normalize();
+    
+    v2=v2-v2.dot(v1)*v1;
+    v2.normalize();
+
+    v3=v3-v3.dot(v2)*v2-v3.dot(v1)*v1;
+    v3.normalize();
+
+    std::cout<<"The eigenvector is: v1 = "<<v1.transpose()<<"\n"
+    <<" v2 = "<<v2.transpose()<<"\n"<<" v3 = "<<v3.transpose()<<std::endl;
 
 
     return 0;
